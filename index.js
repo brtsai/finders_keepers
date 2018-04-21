57144ebb95a5ab66e0cb7ad03e4594600786d751
npartovi <npartovi@gmail.com>
2018-04-20 22:36:53 -0700

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-	res.send({ hi: "there"});
+	res.send({ bye: "buddy"});
 });
 
 // this is the port that heroku will listen on, if none exists, 5000 will take over
