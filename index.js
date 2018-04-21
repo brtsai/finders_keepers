92f98499619c0d76d249ad60fe283684dd1765eb
npartovi <npartovi@gmail.com>
2018-04-20 21:45:47 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+	res.send({ hi: "there"});
+});
+
+// this is the port that heroku will listen on, if none exists, 5000 will take over
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
