15a516d228194e827eba5a0c2ed5b687d03bc2c0
brtsai <bryan@bryantsaiwh.com>
2018-04-23 11:19:29 -0700

    add skeleton for entities, listings reducers so react-redux doesn't break

--- frontend/finders_keepers.jsx
+++ frontend/finders_keepers.jsx
@@ -8,5 +8,6 @@ import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore({});
   ReactDOM.render(<Root store={store} />, root);
 });
