46197b980b1751d78c727e0e4b7a19f43275eed7
npartovi <npartovi@gmail.com>
2018-04-23 11:01:32 -0700

    finished auth flow

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
-module.exports = {
-	googleClientID: "602962130633-091lacaedqh8i52j9m395itd4n9ujt8l.apps.googleusercontent.com",
-	googleClientSecret: "g81YSa1uEJTiDnkswaYGGzCR",
-	mongoURI: "mongodb://finderskeepers:appacademy@ds143678.mlab.com:43678/finders-keepers-dev",
-	cookieKey: 'alksdnoijoijwemncxncoiwencsdkljfnlssdkjnnvjs'
-};
+// keys.js figure out what set of credentials to return
+
+if(process.env.NODE_ENV === 'production'){
+	module.exports = require('./prod');
+}else {
+	module.exports =  require('./dev');
+}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -19,6 +19,7 @@ module.exports = (app) =>{
 	});
 
 	app.get('/api/current_user', (req, res)=>{
+		// res.send(req.session)
 		res.send(req.user);
 	});
 }
