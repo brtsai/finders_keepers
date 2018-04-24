e59836abf5ed88e5f28e5f01d2043f03d10d3bfa
npartovi <npartovi@gmail.com>
2018-04-24 11:03:16 -0700

    created forms

---
+++ frontend/components/forms/add_freebie_form.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+class AddFreebieForm extends React.Component {
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<div>
+				<h1> I am a form</h1>
+			</div>
+		)
+	}
+}
---
+++ frontend/components/forms/add_freebie_form_container.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import AddFreebieForm from './add_freebie_form';
+
+
+const mapStateToProps =(state) => ({
+
+});
+
+const mapDispatchToProps = (dispatch) => ({
+
+});
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps,
+)(AddFreebieForm);
