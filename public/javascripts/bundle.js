a6987c8af4625f1c86b1944ddae9d1de9f514fdb
npartovi <npartovi@gmail.com>
2018-04-24 09:06:13 -0700

    ready for merge

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -26798,6 +26798,7 @@ exports.default = (0, _reactRedux.connect)(mapStateToProps, mapDispatchToProps)(
 /* 114 */
 /***/ (function(module, exports) {
 
+"use strict";
 throw new Error("Module build failed: SyntaxError: Unexpected token (12:4)\n\n\u001b[0m \u001b[90m 10 | \u001b[39m    \u001b[36mreturn\u001b[39m (\n \u001b[90m 11 | \u001b[39m    \t\u001b[33m<\u001b[39m\u001b[33mGoogleButton\u001b[39m \n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 12 | \u001b[39m    ) \t\n \u001b[90m    | \u001b[39m    \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 13 | \u001b[39m  }\n \u001b[90m 14 | \u001b[39m}\n \u001b[90m 15 | \u001b[39m\u001b[0m\n");
 
 /***/ }),
