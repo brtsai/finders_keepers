8333ae22d6f0ed4c6d44d00bf63ce7e081dd5f66
Yujie Zhu <yujiezhu718@gmail.com>
2018-04-23 17:20:34 -0700

    basic navbar template complete

---
+++ frontend/components/navbar/navbar_container.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import NavbarComponent from './navbar_component';
+
+const mapStateToProps = (state) => ({
+
+});
+
+const mapDispatchToProps = (dispatch) => ({
+
+});
+
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+)(NavbarComponent);
