89f985a7e881403da3c6798fb9fa19fb1da23911
npartovi <npartovi@gmail.com>
2018-04-26 10:38:01 -0700

    readded the user model

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+	googleId: String
+});
+
+mongoose.model("users", userSchema);
+
+// This is a test
+//test
