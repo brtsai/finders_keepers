a012305ab5867dc15a69f52e788fb57b9e4067ef
brtsai <bryan@bryantsaiwh.com>
2018-04-27 11:22:07 -0700

    add hooks for receiving new props, adding markers/listings to map

--- frontend/components/map/map.jsx
+++ frontend/components/map/map.jsx
@@ -9,15 +9,33 @@ class Map extends React.Component {
 			center: { lat: latitude, lng: longitude }, // this is SF
 			zoom: 13,
 		};
-
+    console.log(this.props.listings);
 		// wrap the mapDOMNode in a Google Map
 		this.map = new google.maps.Map(this.mapNode, mapOptions);
-
+    
 		const marker = new google.maps.Marker({
 			position: { lat: latitude, lng: longitude },
 			map: this.map,
 		});
 	}
+
+  createNewMarker (lat, lng) {
+    return new google.maps.Marker({
+      position: { lat, lng },
+      map: null
+    });
+  }
+
+  addListingToMap (listing) {
+    if (listing === null) return;
+    console.log('adding listing to map');
+    console.log(listing);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log('receiving new props');
+    console.log(nextProps);
+  }
 
 	render() {
 		return <div className="google-map" ref={map => (this.mapNode = map)} />;
