72675a0801fecd34e07603eb1806f0b6bc93d12a
brtsai <bryan@bryantsaiwh.com>
2018-04-29 20:27:33 -0700

    map openListing to map container

--- frontend/components/map/map_container.jsx
+++ frontend/components/map/map_container.jsx
@@ -1,12 +1,15 @@
 import { connect } from "react-redux";
 
 import Map from "./map";
+import { openListing } from '../../actions/ui_actions';
 
 const mapStateToProps = state => ({
 	listings: state.entities.listings,
 	currentListing: state.ui.currentListing,
 });
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  open: listingId => dispatch(openListing(listingId))
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Map);
